4efdcde621213e35777e269584b7da122c69f82e
pauciulofrancesco-pixel <pauciulofrancesco@gmail.com>
2026-09-02 14:54:03 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='colasanto-client-github-v6-3';
+const CACHE='colasanto-client-github-v6-5';
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./manifest.webmanifest','./icon-192.jpg','./icon-512.jpg','./logo-header.jpg'])).catch(()=>{}))});
 self.addEventListener('activate',e=>e.waitUntil((async()=>{const keys=await caches.keys();await Promise.all(keys.filter(k=>k.startsWith('colasanto-client-github-')&&k!==CACHE).map(k=>caches.delete(k)));await self.clients.claim()})()));
 self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)))});
